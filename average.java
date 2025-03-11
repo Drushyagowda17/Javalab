9c6c8289db90d41a7129755807d689c129bf5385
Drushyagowda17 <drushyagmgowda@gmail.com>
2025-03-11 16:19:40 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,12 @@
+package Java;
+import java.util.Scanner;
+public class Average {
+	public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int n = sc.nextInt(), sum = 0;  
+        for (int i = 0; i < n; i++)
+        	sum += sc.nextInt();  
+        System.out.println(sum / (double) n);  
+    }
+}
